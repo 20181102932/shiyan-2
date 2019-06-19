650f02dedf5d3e6f81e4e5cafec0dc71ab894c41
20181102932 <908907990@qq.com>
2019-06-19 21:00:00 +0800

    shuchubudui
    123

--- shiyan 2/main.cpp
+++ shiyan 2/main.cpp
@@ -1,15 +1,21 @@
-//
-//  main.cpp
-//  shiyan 2
-//
-//  Created by s20181102932 on 2019/6/19.
-//  Copyright © 2019 s20181102932. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<iostream>
+#include<fstream>
+using namespace std;
+struct  student{
+    char name[10];
+    char number[10];
+    char xy[10];
+};
+int main()
+{
+    student a;
+    ifstream b("/Users/s20181102932/Desktop/in.txt");
+    b>>a.xy>>a.number>>a.name;
+    ofstream c("/Users/s20181102932/Desktop/out.txt");
+    if(c.is_open())
+    {
+        c<<a.xy<<a.number<<a.name;
+        c.close();
+    }
     return 0;
 }
